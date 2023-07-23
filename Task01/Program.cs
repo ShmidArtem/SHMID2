db2a90701d7a5e494b96255f30cabcd30b375db2
artem <artemsmidke@gmail.com>
2023-07-23 16:29:04 +0600

    Добавил папку Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Напишите  программу, которая на вход принимает два числа
+// и проверяет, является ли первое число квадратом второго.
+
+
+Cosole. WriteLine("Введите первое целое число");
+int number = Convert.ToInt32(Console.RedLine());
+Cosole. WriteLine("Введите второе целое число");
+int number2 = Convert.ToInt32(Console.RedLine());
+
+if (number1 == numder2 * number2 ) {
+    Console.WriteLine("Квадрат первого числа равен второму числу");
+} else {
+    Console.WriteLine("Квадрат первого числа Не равен второму числу");
+}
